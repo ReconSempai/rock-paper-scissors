16e1b3a52e4bc3745156d06b077022bd8f0a0792
ReconSempai <matiaslimina@gmail.com>
2023-01-11 11:41:02 -0300

    make the function that plays the game

--- javascript.js
+++ javascript.js
@@ -8,14 +8,60 @@ console.log(computerHandNum)
 
 function getComputerChoice(handNum) {
     if (handNum==0) {
-        return "Paper"
+        return "paper"
     }
   else if (handNum==1){
-    return "Rock"
+    return "rock"
  }
  else {
-    return "Scissors"
+    return "scissors"
  }
 }
 let computerHand = getComputerChoice(computerHandNum)
 console.log (computerHand)
+
+let choice = prompt ("What do you chose? Rock, Paper or Scissors?")
+playerSelec= choice.toLowerCase()
+console.log(playerSelec)
+function playRound(playerSelection,computerSelection){
+    
+    switch (playerSelection){
+        case "paper":
+            if (computerSelection === "paper"){
+                return "It's a draw!"
+                
+            } 
+            else if (computerSelection ==="rock"){
+                return "You win! Paper beats Rock"
+            
+            }
+            else {
+                return "You lose! Scissors beat Paper"
+            }
+        case "rock":
+            if (computerSelection === "rock"){
+                return "It's a draw!"
+                
+            } 
+            else if (computerSelection === "scissors"){
+                return "You win! Rock beats Scissors"
+            
+            }
+            else {
+                return "You lose! Paper beat Rock"
+            }
+        case "scissors":
+            if (computerSelection === "scissors"){
+                return "It's a draw!"
+                
+            } 
+            else if (computerSelection === "paper"){
+                return "You win! Scissors beats Paper"
+            
+            }
+            else {
+                return "You lose! Rock beat Scissors"    
+            }
+}
+}
+console.log (playRound(playerSelec,computerHand))
