7949e339768bc573c5656ab908c8da9ea06c3108
ReconSempai <matiaslimina@gmail.com>
2023-01-25 12:12:49 -0300

    add a listener to play the game once

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let min=0
 let max=3
 function getRndHand(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
-  }
+}
 let computerHandNum = getRndHand(min,max);
 console.log(computerHandNum)
 
@@ -11,19 +11,17 @@ function getComputerChoice(handNum) {
     if (handNum==0) {
         return "paper"
     }
-  else if (handNum==1){
-    return "rock"
- }
- else {
-    return "scissors"
- }
+    else if (handNum==1){
+        return "rock"
+    }
+    else {
+        return "scissors"
+    }
 }
 let computerHand = getComputerChoice(computerHandNum)
 console.log (computerHand)
-//Here i use a prompt to make the player chose the option they want to play and then make a simple switch  statment to play the game
-let choice = prompt ("What do you chose? Rock, Paper or Scissors?")
-playerSelec= choice.toLowerCase()
-console.log(playerSelec)
+
+//this function plays the game
 function playRound(playerSelection,computerSelection){
     
     switch (playerSelection){
@@ -34,80 +32,11 @@ function playRound(playerSelection,computerSelection){
             } 
             else if (computerSelection ==="rock"){
                 return "You win! Paper beats Rock"
-            
+                
             }
             else {
                 return "You lose! Scissors beat Paper"
             }
-        case "rock":
-            if (computerSelection === "rock"){
-                return "It's a draw!"
-                
-            } 
-            else if (computerSelection === "scissors"){
-                return "You win! Rock beats Scissors"
-            
-            }
-            else {
-                return "You lose! Paper beat Rock"
-            }
-        case "scissors":
-            if (computerSelection === "scissors"){
-                return "It's a draw!"
-                
-            } 
-            else if (computerSelection === "paper"){
-                return "You win! Scissors beats Paper"
-            
-            }
-            else {
-                return "You lose! Rock beat Scissors"    
-            }
-}
-}
-console.log (playRound(playerSelec,computerHand))
-//Here i make the game function where the game will be played 5 times 
-let roundCount=0
-function game(){
-for (let i=0 ;i<5; i++ ){
-    function getRndHand(min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
-      }
-    let computerHandNum = getRndHand(min,max);
-    console.log(computerHandNum)
-    
-    function getComputerChoice(handNum) {
-        if (handNum==0) {
-            return "paper"
-        }
-      else if (handNum==1){
-        return "rock"
-     }
-     else {
-        return "scissors"
-     }
-    }
-    let computerHand = getComputerChoice(computerHandNum)
-    console.log (computerHand)
-    
-    let choice = prompt ("What do you chose? Rock, Paper or Scissors?")
-    playerSelec= choice.toLowerCase()
-    console.log(playerSelec)
-    function playRound(playerSelection,computerSelection){
-        
-        switch (playerSelection){
-            case "paper":
-                if (computerSelection === "paper"){
-                    return "It's a draw!"
-                    
-                } 
-                else if (computerSelection ==="rock"){
-                    return "You win! Paper beats Rock"
-                
-                }
-                else {
-                    return "You lose! Scissors beat Paper"
-                }
             case "rock":
                 if (computerSelection === "rock"){
                     return "It's a draw!"
@@ -115,7 +44,7 @@ for (let i=0 ;i<5; i++ ){
                 } 
                 else if (computerSelection === "scissors"){
                     return "You win! Rock beats Scissors"
-                
+                    
                 }
                 else {
                     return "You lose! Paper beat Rock"
@@ -127,17 +56,31 @@ for (let i=0 ;i<5; i++ ){
                 } 
                 else if (computerSelection === "paper"){
                     return "You win! Scissors beats Paper"
-                
+                    
                 }
                 else {
                     return "You lose! Rock beat Scissors"    
                 }
-    }
-    }
+            }
+        }
+//Add an event listener for the choice
 
-    console.log (playRound(playerSelec,computerHand))
-    roundCount++
-    console.log(`We just played round number ${roundCount}`)
-}
-}
-console.log (game())
+const btn = document.querySelectorAll('.choice');
+    btn.forEach((choice) => {
+        choice.addEventListener('click', () =>{
+            let playerSelec = choice.id;
+             
+            alert(playRound(playerSelec,computerHand));
+        })
+        
+    })
+   
+    if (typeof playerSelec !== 'undefined'){
+        alert(`esta definida,${playerSelec}`)
+    };
+    
+        
+        
+        
+
+        
